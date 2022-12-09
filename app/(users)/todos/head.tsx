1e94db56b7c22a471c565725e1b96895b602b2c5
Paul Buscano <pbuscano@deloitte.com>
2022-12-09 16:04:20 +0800

    Initial commit

---
+++ app/(users)/todos/head.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Head() {
+  return (
+    <>
+      <title>todos page</title>
+    </>
+  );
+}
+
+export default Head;
---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Head() {
+  return (
+    <>
+      <title>Default page</title>
+    </>
+  );
+}
+
+export default Head;
